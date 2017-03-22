b3682368e48b0c622ba7fc46fcc522fc9ba6508a
sdjogi <jogijogi123>
2017-03-22 13:35:59 +0000

    s

--- test/src/main/java/com/main/app/CaptchaController.java
+++ test/src/main/java/com/main/app/CaptchaController.java
@@ -35,18 +35,6 @@ public class CaptchaController {
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
     public Response getImage() {
         File outputfile = new File("C:\\Users\\jogish\\Desktop\\saved.png");
-        ;
-        //  SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        //  Calendar c =  Calendar.getInstance();
-        //  try {
-        //      c.setTime(sdf.parse("21/09/2016"));
-        //  } catch (ParseException e) {
-        //      e.printStackTrace();
-        //  }
-        //  c.add(Calendar.DAY_OF_MONTH, 10);  // 10 days after Term3 start date.
-        //  String ndate = sdf.format(c.getTime());
-        //
-        //  System.out.println(ndate);
         try{
 
             int minWordLength = 4;
@@ -54,22 +42,17 @@ public class CaptchaController {
             int fontSize = 20;
             int imageWidth = 100;
             int imageHeight = 36;
-
             final ImageDeformation backDef = new ImageDeformationByFilters(new java.awt.image.ImageFilter[] {});
             final ImageDeformation textDef = new ImageDeformationByFilters(new java.awt.image.ImageFilter[] {});
             final ImageDeformation postDef = new ImageDeformationByFilters(new java.awt.image.ImageFilter[] {});
-
             TextPaster randomPaster = new DecoratedRandomTextPaster(minWordLength, maxWordLength,
                     new RandomListColorGenerator(new java.awt.Color[] { new java.awt.Color(23, 170, 27),
                             new java.awt.Color(220, 34, 11), new java.awt.Color(23, 67, 172) }), new TextDecorator[] {});
             BackgroundGenerator background = new UniColorBackgroundGenerator(imageWidth, imageHeight,
                     java.awt.Color.white);
             FontGenerator font = new RandomFontGenerator(fontSize, fontSize);
-
-            //word2image 1
             final WordToImage word2image = new DeformedComposedWordToImage(font, background, randomPaster, backDef,
                     textDef, postDef);
-
             java.awt.image.BufferedImage img = word2image.getImage(getCaptcha());
 
             ImageIO.write(img, "png", outputfile);
@@ -84,7 +67,6 @@ public class CaptchaController {
     String getCaptcha() {
         Random random = new Random();
         int length = 4;
-        //BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
         StringBuffer captchaStringBuffer = new StringBuffer();
         for (int i = 0; i < length; i++){
             int captchaNumber = Math.abs(random.nextInt()) % 60;
